4624c983e06062d78bdd31eb3f9f77a39deb8c3f
kubo <jsodpad@gmail.com>
2023-12-06 23:37:33 +0100

    2023 Day 5 V2

--- 2023/src/main/kotlin/Day5.kt
+++ 2023/src/main/kotlin/Day5.kt
@@ -34,21 +34,41 @@ class Day5(input: String) : StatefulPuzzle<Long, Long>(input) {
         .map { humidityToLocation.map(it) }
         .min()
 
-    override fun solvePart2(): Long = seedRanges.asSequence()
-        .flatMap { it.asSequence() }
-        .map { seedToSoil.map(it) }
-        .map { soilToFertilizer.map(it) }
-        .map { fertilizerToWater.map(it) }
-        .map { waterToLight.map(it) }
-        .map { lightToTemperature.map(it) }
-        .map { temperatureToHumidity.map(it) }
-        .map { humidityToLocation.map(it) }
-        .minOf { it }
+    override fun solvePart2(): Long = humidityToLocation
+        .map(
+            temperatureToHumidity
+                .map(
+                    lightToTemperature
+                        .map(
+                            waterToLight
+                                .map(
+                                    fertilizerToWater
+                                        .map(
+                                            soilToFertilizer
+                                                .map(
+                                                    seedToSoil
+                                                        .map(seedRanges)
+                                                )
+                                        )
+                                )
+                        )
+                )
+        ).minOf { it.first }
 
     data class Category(val maps: List<CategoryMap>) {
         fun map(value: Long) = maps.firstOrNull { it.contains(value) }?.map(value) ?: value
-        fun map(range: LongRange): Set<LongRange> {
-            return setOf(0..1L)
+        fun map(inputRanges: List<LongRange>): List<LongRange> {
+            var unmappedInputRanges = inputRanges.toMutableList()
+            val unprocessedMaps = ArrayDeque(maps)
+            val processedInputRanges = mutableListOf<LongRange>()
+            while (unprocessedMaps.isNotEmpty()) {
+                val map = unprocessedMaps.removeFirst()
+                val remainderInputRanges = mutableListOf<LongRange>()
+                unmappedInputRanges.forEach { map.intersect(it).forEach { processedInputRanges.add(map.map(it)) } }
+                unmappedInputRanges.forEach { map.remainders(it).forEach { remainderInputRanges.add(it) } }
+                unmappedInputRanges = remainderInputRanges
+            }
+            return processedInputRanges + unmappedInputRanges
         }
     }
 
@@ -58,10 +78,18 @@ class Day5(input: String) : StatefulPuzzle<Long, Long>(input) {
         fun map(value: Long) = value + diff
         fun contains(value: Long) = sourceStart <= value && value < sourceStart + range
 
-        fun intersect(range: LongRange): LongRange? {
-            return (0..1L)
-        }
-        fun map(range: LongRange): LongRange = range.first + diff..<range.last + diff
+        fun intersect(inputRange: LongRange): List<LongRange> = listOf(
+            (maxOf(inputRange.first, sourceStart)..minOf(inputRange.last, sourceStart + range - 1))
+        ).filter { !it.isEmpty() }
+
+        fun remainders(inputRange: LongRange): List<LongRange> = listOf(
+            (minOf(inputRange.first, sourceStart)..<minOf(inputRange.last + 1, sourceStart)),
+            (maxOf(inputRange.first, sourceStart + range)..<maxOf(inputRange.last + 1, sourceStart + range)),
+        )
+            .filter { !(it.first == inputRange.first && it.last == inputRange.last) || !(it.first == sourceStart && it.last == sourceStart + range - 1) }
+            .filter { !it.isEmpty() }
+
+        fun map(range: LongRange): LongRange = range.first + diff..range.last + diff
     }
 
     fun String.mapToCategoryMap(): CategoryMap {
