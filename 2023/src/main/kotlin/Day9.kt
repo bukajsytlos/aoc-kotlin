a81470577a42bc2035a2b277c908cf45647a3fbb
kubo <jsodpad@gmail.com>
2023-12-09 12:38:38 +0100

    2023 Day 9 recursive

--- 2023/src/main/kotlin/Day9.kt
+++ 2023/src/main/kotlin/Day9.kt
@@ -6,15 +6,6 @@ class Day9(input: String) : StatefulPuzzle<Int, Int>(input) {
 
     override fun solvePart2(): Int = oasisHistories.map { it.reversed() }.sumOf { it.nextValue() }
 
-    private fun List<Int>.nextValue(): Int {
-        val lastDiffs = mutableListOf<Int>()
-        lastDiffs.add(this.last())
-        var diffSequence: List<Int> = this
-        while (diffSequence.any { it != 0 }) {
-            diffSequence = diffSequence.zipWithNext { a, b -> b - a }
-            lastDiffs.add(diffSequence.last())
-        }
-
-        return lastDiffs.sum()
-    }
+    private fun List<Int>.nextValue(): Int = if (this.all { it == 0 }) 0 else
+        this.last() + this.zipWithNext { a, b -> b - a }.nextValue()
 }
