fe6d8cb8c59a952d8de5eccbbd98614b4e31a085
kubo <jsodpad@gmail.com>
2024-12-01 21:20:11 +0100

    2024 Day 1 pimp

--- 2024/src/main/kotlin/Day1.kt
+++ 2024/src/main/kotlin/Day1.kt
@@ -1,7 +1,7 @@
 import kotlin.math.abs
 
 class Day1(input: String) : StatefulPuzzle<Int, Int>(input) {
-    val locationIds = input.lines().map { line -> line.split("   ") }.map { Pair(it[0].toInt(), it[1].toInt()) }.unzip()
+    val locationIds = input.lines().map { line -> line.split("   ").let { it[0].toInt() to it[1].toInt()} }.unzip()
     val list1 = locationIds.first.sorted()
     val list2 = locationIds.second.sorted()
 
@@ -10,7 +10,7 @@ class Day1(input: String) : StatefulPuzzle<Int, Int>(input) {
     }
 
     override fun solvePart2(): Int {
-        val weightMap = list2.groupBy { it }.mapValues { it.value.size }
-        return list1.sumOf { it * weightMap.getOrElse(it) { 0 } }
+        val weightMap = list2.groupingBy { it }.eachCount()
+        return list1.sumOf { it * (weightMap[it] ?: 0) }
     }
 }
