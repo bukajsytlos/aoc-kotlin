fe30ba749d7c61035afd0ed8cedaa76e42694890
kubo <jsodpad@gmail.com>
2025-12-03 21:14:28 +0100

    2025 Day 2 pimp

--- 2025/src/main/kotlin/Day2.kt
+++ 2025/src/main/kotlin/Day2.kt
@@ -1,17 +1,17 @@
 class Day2(input: String) : StatefulPuzzle<Long, Long>(input) {
     val ids = input.split(",")
-        .flatMap { LongRange(it.substringBefore("-").toLong(), it.substringAfter("-").toLong()).asIterable() }
+        .flatMap { LongRange(it.substringBefore("-").toLong(), it.substringAfter("-").toLong()) }
 
 
     override fun solvePart1(): Long {
-        val repeatDigitsPattern = """^(\d+)\1$""".toPattern()
-        return ids.filter { repeatDigitsPattern.matcher(it.toString()).matches() }
+        val repeatDigitsPattern = """^(\d+)\1$""".toRegex()
+        return ids.filter { repeatDigitsPattern.matches(it.toString()) }
             .sum()
     }
 
     override fun solvePart2(): Long {
-        val repeatDigitsPattern = """^(\d+)\1+$""".toPattern()
-        return ids.filter { repeatDigitsPattern.matcher(it.toString()).matches() }
+        val repeatDigitsPattern = """^(\d+)\1+$""".toRegex()
+        return ids.filter { repeatDigitsPattern.matches(it.toString()) }
             .sum()
     }
 }
